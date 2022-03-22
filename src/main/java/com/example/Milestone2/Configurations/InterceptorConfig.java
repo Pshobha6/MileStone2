446f4be68f1d522edd2c20fc2accb7b93c69aa12
pshobha6 <pshobha6@deloitte.com>
2022-03-22 13:16:20 +0530

    Added Interceptor

---
+++ src/main/java/com/example/Milestone2/Configurations/InterceptorConfig.java
@@ -0,0 +1,16 @@
+package com.example.Milestone2.Configurations;
+
+import com.example.Milestone2.AuthenticationInterceptor.AuthInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        WebMvcConfigurer.super.addInterceptors(registry);
+        registry.addInterceptor(new AuthInterceptor());
+    }
+}
