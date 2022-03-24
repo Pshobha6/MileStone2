db8db532953c8adcaa20c4a0fc9d448d69d80a26
pshobha6 <pshobha6@deloitte.com>
2022-03-25 00:10:27 +0530

    Time header in response

--- src/main/java/com/example/Milestone2/Interceptor/AuthInterceptor.java
+++ src/main/java/com/example/Milestone2/Interceptor/AuthInterceptor.java
@@ -1,7 +1,9 @@
 package com.example.Milestone2.Interceptor;
 
 
+import org.springframework.lang.Nullable;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,11 +22,17 @@ public class AuthInterceptor implements HandlerInterceptor, BasicAuthInterceptor
 //            return false;
 //        }
 //    }
-
+@Override
+public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+    long time = (long)request.getAttribute("startTime");
+    long timeTaken = System.currentTimeMillis() - time;
+    System.out.println("The time taken for Processing the request is "+timeTaken+" ms");
+}
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         System.out.println("*****Inside Prehandle********");
         String authHeader = request.getHeader("X-Auth-Token");
+        request.setAttribute("startTime",System.currentTimeMillis());
        // if (authHeader == null||authHeader.isEmpty()||!authHeader.equalsIgnoreCase("Token")) {
         if (authHeader == null||authHeader.isEmpty()) {
             response.setContentType("application/json");
